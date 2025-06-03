ccf60d76b07cd97b0b4b109fff4d69067514e84f
Billie45 <billiezandraw@gmail.com>
2025-06-03 08:17:13 +0700

    Coba Hosting Vercel 2
    Perbaikan tampilan lagi lagi

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,6 +23,7 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
         <!-- Tambahkan stack styles -->
         @stack('styles')
 
